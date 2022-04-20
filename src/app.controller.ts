03f85a1da34dd3d46203f19b6a790eb319a4d77f
myungsangBaek <bms6168@naver.com>
2022-04-20 17:01:27 +0900

    [FEATURE] Generate request object

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Request } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Req() req: Request): string {
+    console.log(req);
     return this.appService.getHello();
   }
 }
