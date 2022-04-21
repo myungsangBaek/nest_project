6792dfdfa05e9b646dea4f0fc82a6d4065e855a4
myungsangBaek <bms6168@naver.com>
2022-04-21 11:22:38 +0900

    [FEATURE] Generate sub domain

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,8 @@
-import { Request } from '@nestjs/common';
+import { HostParam, Request } from '@nestjs/common';
 import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('app')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
